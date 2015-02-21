dee1fa29d86f422f29c909f2fa9ed8c854870c36
Tommy Janszen <tommy.janszen@gmail.com>
2015-02-21 06:45:06 -0800

    Add asc and desc sort

--- server/routes/albums/index.js
+++ server/routes/albums/index.js
@@ -4,7 +4,14 @@ var Album = require('../../models/album');
 
 module.exports = {
   handler: function(request, reply) {
-    Album.find({}).sort({date:1}).exec(function(err, albums) {
+    console.log(request.query.order)
+    var sorting = -1
+    if(request.query.order === 'Ascending') {
+      sorting = 1
+    }else {
+      sorting = -1
+    }
+    Album.find({}).sort({date:sorting}).exec(function(err, albums) {
       reply.view('templates/albums/index', {path: '/albums', albums:albums});
     });
   }
